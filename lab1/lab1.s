0981e31510f543d2111cacc62c778456d4bb94f8
Conor <conormag94@gmail.com>
2015-09-30 11:37:12 +0100

    Wrote C program. Created x86 assembly file

---
+++ lab1/lab1.s
@@ -0,0 +1,168 @@
+	.file	"lab1.c"
+	.text
+	.globl	sort
+	.type	sort, @function
+sort:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -12(%rbp)
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L6:
+	movl	$0, -8(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L3
+.L5:
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %edx
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, %edx
+	jge	.L4
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -4(%rbp)
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rcx
+	movq	-24(%rbp), %rax
+	addq	%rcx, %rax
+	movl	(%rax), %eax
+	movl	%eax, (%rdx)
+	movl	-8(%rbp), %eax
+	cltq
+	addq	$1, %rax
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+.L4:
+	addl	$1, -8(%rbp)
+.L3:
+	movl	-28(%rbp), %eax
+	subl	$1, %eax
+	cmpl	-8(%rbp), %eax
+	jg	.L5
+	addl	$1, -12(%rbp)
+.L2:
+	movl	-12(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L6
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	sort, .-sort
+	.section	.rodata
+.LC0:
+	.string	"%d "
+	.text
+	.globl	printArray
+	.type	printArray, @function
+printArray:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L8
+.L9:
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	addl	$1, -4(%rbp)
+.L8:
+	movl	-4(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jl	.L9
+	movl	$10, %edi
+	call	putchar
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	printArray, .-printArray
+	.globl	main
+	.type	main, @function
+main:
+.LFB4:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movl	$5, -48(%rbp)
+	movl	$65, -44(%rbp)
+	movl	$40, -40(%rbp)
+	movl	$39, -36(%rbp)
+	movl	$30, -32(%rbp)
+	movl	$8, -28(%rbp)
+	movl	$17, -24(%rbp)
+	movl	$21, -20(%rbp)
+	movl	$3, -16(%rbp)
+	movl	$10, -12(%rbp)
+	leaq	-48(%rbp), %rax
+	movl	$10, %esi
+	movq	%rax, %rdi
+	call	printArray
+	leaq	-48(%rbp), %rax
+	movl	$10, %esi
+	movq	%rax, %rdi
+	call	sort
+	leaq	-48(%rbp), %rax
+	movl	$10, %esi
+	movq	%rax, %rdi
+	call	printArray
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE4:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
